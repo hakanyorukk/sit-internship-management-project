71e30e6513845e3b31c9c8a42b1130e166acd2e7
hakanyorukk <hakanyorukomer@hotmail.com>
2026-06-29 10:15:55 +0300

    initial setup

---
+++ src/main/java/org/example/internship_system/InternshipSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.internship_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipSystemApplication.class, args);
+    }
+
+}
