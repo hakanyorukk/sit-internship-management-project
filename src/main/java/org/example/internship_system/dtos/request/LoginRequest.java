81d90edef067355abb841980cfe7436231f89fe6
mtlzeynep <zeynep.mutlu6847@gmail.com>
2026-07-15 11:56:27 +0300

    feat: change to lombok getter setter

--- src/main/java/org/example/internship_system/dtos/request/LoginRequest.java
+++ src/main/java/org/example/internship_system/dtos/request/LoginRequest.java
@@ -2,7 +2,11 @@ package org.example.internship_system.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class LoginRequest {
 
     @NotBlank(message = "Email is required")
@@ -14,19 +18,4 @@ public class LoginRequest {
 
     public LoginRequest() {}
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/org/example/internship_system/entity/Company.java
+++ src/main/java/org/example/internship_system/entity/Company.java
@@ -1,7 +1,11 @@
 package org.example.internship_system.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "companies")
 public class Company {
@@ -27,59 +31,4 @@ public class Company {
 
     public Company() {}
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
 }
